96702e2bc4b52fd3166a570ce836b975f27ef639
edsilv <e.silverton@gmail.com>
2014-11-10 12:40:15 +0000

    get iframe contents

--- e2e/features/MetaData.js
+++ e2e/features/MetaData.js
@@ -14,32 +14,67 @@ var Metadata = function() {
 		// http://assertselenium.com/2013/02/22/handling-iframes-using-webdriver/
 		ptor = protractor.getInstance();
 
+		//ptor.get('/examples/monograph.html');
+
+		//browser.driver.get('/examples/monograph.html');
+		////browser.driver.sleep(4000);
+		//var app = browser.findElement(protractor.By.id('app'));
+        //
+		//var firstItem = app.get(1)
+        //
+		//console.log(app);
+
 		ptor.get('/examples/monograph.html').then(function(){
 
 			//ptor.getTitle().then(function(val){
 			//	console.log(val);
 			//});
 
-			ptor.findElement(protractor.By.css('.wellcomePlayer')).then(function(el) {
-				callback();
-			}, function(){
-				callback.fail("wellcomePlayer div not found");
+			ptor.sleep(3000).then(function(){
+				ptor.findElement(protractor.By.tagName('iframe')).then(function(iframe) {
+					console.log('the iframe: ' + iframe);
+					callback();
+				}, function(){
+					callback.fail("iframe not found");
+				});
 			});
+
+			//ptor.findElement(protractor.By.css('.wellcomePlayer')).then(function(el) {
+			//	callback();
+			//}, function(){
+			//	callback.fail("wellcomePlayer div not found");
+			//});
 
 		});
 	});
 
 	this.When(/^they click "([^"]*)"$/, function (arg1, callback) {
-		ptor.findElementInFrame(ptor,protractor.By.buttonText(arg1)).then(function(el){
-				el.click().then(function() {
-					callback();
-		});
-		});
+
+		callback();
+
+		//ptor.findElementInFrame(ptor,protractor.By.buttonText(arg1)).then(function(el){
+		//		el.click().then(function() {
+		//			callback();
+		//});
+		//});
 	});
 
 	this.Then(/^metadata key\/value pairs are displayed to the user$/, function (callback) {
-		// Write code here that turns the phrase above into concrete actions
-		callback.pending();
+
+		ptor.switchTo().frame(0);
+
+		ptor.findElement(protractor.By.id('app')).then(function(app) {
+			console.log('the app: ' + app);
+
+			app.getAttribute("class").then(function(c) {
+				console.log("the class: " + c);
+				callback();
+			});
+
+		}, function(){
+			callback.fail("app not found");
+		});
+
 	});
 
 	this.Then(/^the metadata side panel is visible to the user$/, function (callback) {
